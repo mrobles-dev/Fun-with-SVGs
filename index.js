ec97a6fb2101ec8c5c24ab5ed112875a2749d63e
mrobles-dev <miltonjrobles1996@gmail.com>
2023-05-17 22:53:06 -0500

    updated index.js to function

--- index.js
+++ index.js
@@ -1,11 +1,92 @@
 const inquirer = require('inquirer');
 const {Circle, Triangle, Square} = require ('./lib/shapes')
-
+const fs = require('fs');
+const { File } = require('buffer');
 
 
 
 const svgDoc = ({ color, shape, text }) =>
   `
+<svg version="1.1"
+     width="300" height="200"
+     xmlns="http://www.w3.org/2000/svg">
+
+  <rect width="100%" height="100%" fill= "${color}" />
+
+  <"${shape}" cx="150" cy="100" r="80" fill="green" />
+
+  <${text} x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+</svg>
+`;
 
 
-`
+function writeToFile(fileName, res){
+  let svg = "";
+  svg =
+    '<svg version="1.1" width="700" height="500" xmlns="http://www.w3.org/2000/svg">';
+  svg += "<g>";
+  svg += `${res.shape}`;
+
+  // Closing </g> tag
+  svg += "</g>";
+  // Closing </svg> tag
+  svg += "</svg>";
+  console.log(res.color);
+
+  let shapeChoice;
+  if (res.shape === Triangle) {
+    shapeChoice = new Triangle();
+    svg += ` <polygon points="150, 18 244, 182 56, 182" fill=“${res.color}”/>
+    `;
+  } else if (res.shape === Square) {
+    shapeChoice = new Square();
+    svg += ` <rect x="73" y="40" width="160" height="160" fill=“${res.color}"/>
+    `;
+  } else if (res.shape === Circle) {
+    shapeChoice = new Circle();
+    svg += ` <circle cx="150" cy="115" r="80" fill=“${res.color}”/>
+    `;
+  }
+
+
+
+
+  
+  fs.writeFile(fileName, svg, (err) => {
+    err ? console.log(err) : console.log("generated logo");
+  });
+}
+
+function userPrompt(){
+
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What shape would you like to ?",
+        name: "shape",
+        choices: ["square", "triangle", "circle"],
+      },
+      {
+        type: "list",
+        message: "What color would you like to ?",
+        name: "color",
+        choices: ["red", "blue", "green" ],
+      },
+      {
+        type: "input",
+        message: "What what 3 letters would you like to display?",
+        name: "text"
+      },
+  
+    ]).then((res)=> {   
+      writeToFile("logo.svg", res);
+
+    })
+}
+  userPrompt();
+
+
+
+
